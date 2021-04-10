dbcefc44aa1d06003ad0bc43576d1dae314d0aa4
Abtses <70921388+Abtses@users.noreply.github.com>
2021-04-10 18:54:05 -0300

    Add files via upload

---
+++ src/Operaciones.h
@@ -0,0 +1,18 @@
+/*
+ * Operaciones.h
+ *
+ *  Created on: 26 de mar. de 2021
+ *      Author: Abel
+ */
+
+#ifndef OPERACIONES_H_
+#define OPERACIONES_H_
+
+int ingresarNumero();
+int suma(int operandoUno,int operandoDos);
+int resta(int operandoUno, int operandoDos);
+float division(float operandoUno, float operandoDos);
+int multiplicacion(int operandoUno, int operandoDos);
+int factorial(int operando);
+
+#endif /* OPERACIONES_H_ */
